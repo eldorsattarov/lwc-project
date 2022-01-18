0f887d38bf9eeebe92663969527414c7c568a193
admin <sattaroveldor@gmail.com>
2022-01-18 21:30:30 +0500

    ox task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import "./sass/main.scss";
-import Navbar from "./components/Navbar";
-import DesignService from "./components/DesignService";
-import TechnicalService from "./components/TechnicalService";
-import About from "./components/About";
-import Main from "./components/Main";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Contact from "./components/Contact";
+
 function App() {
   return (
-     <body id="body">
      <BrowserRouter>
-       <Navbar/>
        <Switch>
-         <Route path="/" exact component={Main}/>
-         <Route path="/about" exact component={About}/>
-         <Route path="/design" exact component={DesignService}/>
-         <Route path="/technical" exact component={TechnicalService}/>
-         <Route path="/contact" exact component={Contact}/>
+
        </Switch>
      </BrowserRouter>
-     </body>
   );
 }
 
